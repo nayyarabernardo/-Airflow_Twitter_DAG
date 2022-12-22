99ee9556536ca298f6cb097b438fb26845cabae3
Nayara <99041470+nayyarabernardo@users.noreply.github.com>
2022-12-22 01:30:37 -0300

    Update twitter_dag.py

--- airflow_pipeline/dags/twitter_dag.py
+++ airflow_pipeline/dags/twitter_dag.py
@@ -13,7 +13,7 @@ with DAG(dag_id = "TwitterDAG", start_date=datetime.now(), schedule_interval="@d
     start_time = (datetime.now() + timedelta(-1)).date().strftime(TIMESTAMP_FORMAT)
     query = "datascience"
     to = TwitterOperator(file_path=join("datalake/twitter_datascience",
-                                        "extract_date={{ ds }}",  #informacoes em tempo de rodar o codigo
-                                        f"datascience_{{ ds_nodasg }}.json"),  #substituir pela data sem barra
+                                        "extract_date={{ ds }}",  
+                                        f"datascience_{{ ds_nodasg }}.json"),  
                                         query=query, start_time=start_time, end_time=end_time, task_id="twitter_datascience")
        
